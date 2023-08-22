d7832860d34a61bab4b47ffc1036ac3cfed4bc76
Sandeep Bypina <sandeep@postorder.io>
2023-08-22 19:36:16 +0530

    initial open cart files

---
+++ pb_root/config.php
@@ -0,0 +1,36 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/pb_root/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_OPENCART', '/usr/local/var/www/pb/');
+define('DIR_APPLICATION', DIR_OPENCART . 'pb_root/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/usr/local/var/www/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'bypina107');
+define('DB_DATABASE', 'pb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'pb_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
